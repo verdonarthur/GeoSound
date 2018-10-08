0e67a1c95aad7bf122df86c04730d13b212013ef
Arthur Verdon <arthur.verdon@heig-vd.ch>
2018-10-08 13:49:17 +0200

    add config file in repo

--- config/_app.conf.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    urlDatabase:'' // example : 'mongodb://root:toor@192.168.99.100:32768/exampleProject?authSource=admin',
-    
-    // other configuration...
-}
-
---
+++ config/app.conf.js
@@ -0,0 +1,6 @@
+module.exports = {
+    urlDatabase: process.env.MONGODB_URI || 'mongodb://root:toor@192.168.99.100:32768/exampleProject?authSource=admin',
+    
+    // other configuration...
+}
+
