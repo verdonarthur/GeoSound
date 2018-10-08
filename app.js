a3aa27484d2114d8c4c7e87edccc4252046b4fcb
Arthur Verdon <arthur.verdon@heig-vd.ch>
2018-10-08 13:39:54 +0200

    add URL adaptation for heroku in mongo

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/app.conf.js')
 const app = express()
 const port = 3000
 
@@ -6,7 +8,8 @@ const port = 3000
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
-app.use(express.static('./public'))
+/*+++++++++++++++MONGOOSE CONFIGURATION++++++++++++++++++*/
+mongoose.connect(config.urlDatabase);
 
 /*+++++++++++++++API ROUTE LOADING++++++++++++++++++*/
 // example : app.use('/api/user', user)
