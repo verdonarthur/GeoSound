a370134bf9508f6b7a9e250674d543e33e2b8ce4
Arthur Verdon <arthur.verdon@heig-vd.ch>
2018-10-15 10:00:18 +0200

    Add error middleware manager and adapt to it in soundrouter

--- app.js
+++ app.js
@@ -15,9 +15,56 @@ mongoose.connect(config.urlDatabase);
 // example : app.use('/api/user', user)
 app.use('/api/sound', require('./routes/soundRouter'))
 
-app.get('/', (req,res)=>{
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// API error handler (responds with JSON)
+app.use('/api', (err, req, res, next) => {
+
+    // Log the error on stderr
+    console.warn(err);
+
+    // Respond with 422 Unprocessable Entity if it's a Mongoose validation error
+    if (err.name == 'ValidationError' && !err.status) {
+        err.status = 422;
+    }
+
+    // Set the response status code
+    res.status(err.status || 500);
+
+    // Send the error message in the response
+    const response = {
+        message: err.message
+    };
+
+    // If it's a validation error, also send the errors details from Mongoose
+    if (err.status == 422) {
+        response.errors = err.errors;
+    }
+
+    // Send the error response
+    res.send(response);
+});
+
+// Generic error handler (responds with HTML)
+app.use((err, req, res, next) => {
+
+    // Set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // Render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.get('/', (req, res) => {
     res.send('application is launched')
 });
 
-app.listen(port, 
+app.listen(port,
     () => console.log(`App listening on port : ${port}!`))
--- routes/soundRouter.js
+++ routes/soundRouter.js
@@ -11,59 +11,66 @@ const LogUtils = require('../app/utils/LogUtils')
  */
 async function loadSoundFromParam(req, res, next) {
     try {
-        res.sound = await SoundController.getASound(req.params.id)
-
+        res.sound = await SoundController.getASound(req.params.id)        
         next()
     } catch (err) {
-        console.log(err)
-        return res.status(400).send('Sound not found')
+        return next(err)
     }
 }
 
 /**
  * Get all sounds from the db
  */
-router.get('/', async (req, res) => {
-    res.send(await SoundController.getAllSound())
+router.get('/', async (req, res, next) => {
+    try {
+        res.send(await SoundController.getAllSound())
+    } catch (err) {
+        return next(err)
+    }
 })
 
 /**
  * Retrieve a sound by his id
  */
-router.get('/:id', loadSoundFromParam, async (req, res) => {    
-    res.send(res.sound)
+router.get('/:id', loadSoundFromParam, async (req, res, next) => {
+    try {
+        res.send(res.sound)
+    } catch (err) {
+        return next(err)
+    }
+
 })
 
 /**
  * Post a sound in the db
  */
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         res.send(await SoundController.postASound(req.body))
     } catch (err) {
-        LogUtils.logAndSendError500(res, err)
+        return next(err)
     }
 })
 
 /**
  * Update a sound in db
  */
-router.put('/:id', loadSoundFromParam, async (req, res) => {
+router.put('/:id', loadSoundFromParam, async (req, res, next) => {
     try {
         res.send(await SoundController.putASound(req.params.id, req.body))
     } catch (err) {
-        LogUtils.logAndSendError500(res, err)
+        return next(err)
     }
 })
 
 /**
  * Remove a sound in db
  */
-router.delete('/:id', loadSoundFromParam, async (req, res) => {
+router.delete('/:id', loadSoundFromParam, async (req, res, next) => {
     try {
         res.send(await SoundController.deleteASound(res.sound))
     } catch (err) {
-        LogUtils.logAndSendError500(res, err)
+        next(err)
     }
 })
 
